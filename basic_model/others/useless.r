37fc3dbaf287870fa6d9e0595c5f2a175fdf0c2d
Alexandre S <alexselvestrel@hotmail.fr>
2024-11-27 17:24:18 +0100

    almost done

--- basic_model/others/useless.r
+++ basic_model/others/useless.r
@@ -28,12 +28,12 @@ path_data <- "../data"
 
 df <- read_excel(glue("{path_data}/radiomiques_global.xlsx"))
 df$key <- paste0(df$classe_name, "_", df$patient_num)
-df <- df[!df$key %in% c("CCK_16", "CHC_204"), ]
-df <- df[df$classe_name == "Mixtes", ]
+# df <- df[!df$key %in% c("CCK_16", "CHC_204"), ]
+df <- df[df$classe_name != "Mixtes", ]
+
 
 
 df <- df[, c("key", "temps_inj", colnames(df)[grepl("original", colnames(df))])]
-
 df_art <- df[df$temps_inj == "ART", ]
 colnames(df_art)[2:ncol(df_art)] <- paste0(colnames(df_art)[2:ncol(df_art)], "_ART")
 df_port <- df[df$temps_inj == "PORT", ]
@@ -43,12 +43,15 @@ colnames(df_vein)[2:ncol(df_art)] <- paste0(colnames(df_vein)[2:ncol(df_art)], "
 df_tard <- df[df$temps_inj == "TARD", ]
 colnames(df_tard)[2:ncol(df_art)] <- paste0(colnames(df_tard)[2:ncol(df_art)], "_TARD")
 
+
 df <- merge(df_art, df_port, by = "key")
+print(dim(df))
 # df <- merge(df, df_vein, by = "key")
 df <- merge(df, df_tard, by = "key")
 
 
 
-df <- na.omit(df)
+# afficher les key de ceux qui ont des na
+print(df[apply(df, 1, function(x) any(is.na(x))), "key"])
 # write_xlsx(df, "../data/radiomiques_global_check.xlsx")
 print(nrow(df))
